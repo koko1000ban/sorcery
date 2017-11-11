20a781f564ed53adda8f7963656e290a4ea286ff
大谷旅人 <tabito.ohtani@gmail.com>
2017-11-11 23:17:39 +0900

    Support v2.1

--- lib/sorcery/providers/line.rb
+++ lib/sorcery/providers/line.rb
@@ -9,15 +9,16 @@ module Sorcery
     class Line < Base
       include Protocols::Oauth2
 
-      attr_accessor :token_url, :user_info_path, :auth_path
+      attr_accessor :token_url, :user_info_path, :auth_path, :auth_url
 
       def initialize
         super
 
         @site           = 'https://access.line.me'
         @user_info_path = 'https://api.line.me/v2/profile'
-        @token_url      = 'https://api.line.me/v2/oauth/accessToken'
-        @auth_path      = 'dialog/oauth/weblogin'
+        @token_url      = 'https://api.line.me/oauth2/v2.1/token'
+        @auth_path      = 'oauth2/v2.1/authorize'
+        @auth_url       = 'https://access.line.me/oauth2/v2.1/authorize'
       end
 
       def get_user_hash(access_token)
@@ -28,11 +29,26 @@ module Sorcery
         end
       end
 
+      def authorize_url(options = {})
+
+        oauth_params = {
+          response_type: 'code',
+          client_id: @key,
+          redirect_uri: @callback_url,
+          scope: 'openid profile',
+          nonce: SecureRandom.hex(10),
+          state: SecureRandom.hex(10)
+        }
+
+        "#{options[:authorize_url]}?#{oauth_params.to_query}"
+      end
+
       # calculates and returns the url to which the user should be redirected,
       # to get authenticated at the external provider's site.
       def login_url(_params, _session)
-        authorize_url(authorize_url: auth_path)
+        authorize_url(authorize_url: auth_url)
       end
+
       # tries to login the user from access token
       def process_callback(params, _session)
         args = {}.tap do |a|
